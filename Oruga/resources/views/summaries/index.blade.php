c3a65d06c2e2573022e23738607bc28ce029f335
南健士郎 <nsr.minami0328@gmail.com>
2019-02-26 17:25:33 +0900

    フロント雛形+ルーティング

--- Oruga/resources/views/summaries/index.blade.php
+++ Oruga/resources/views/summaries/index.blade.php
@@ -1,1 +1,16 @@
+<!doctype html>
+<html lang = "ja">
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" href="style.css">
+            <title>Oruga</title>
+        </head>
 
+        <body>
+            <div class="message-box">
+            </div>
+            <div class="chat-box">
+            </div>
+        </body>
+    </html>
