cd038c4b289e9af39516f42fbfd00598b6c4522b
南健士郎 <nsr.minami0328@gmail.com>
2019-03-12 15:13:31 +0900

    vue.js 要約/APIほか

--- Oruga/resources/views/summaries/index.blade.php
+++ Oruga/resources/views/summaries/index.blade.php
@@ -1,34 +1,36 @@
 <!doctype html>
 <html>
-
     <body>
-        <div id="chat">
-            <input type="text" v-model="word">
-            <button type="button" @click="onClick">要約検索</button></br>
-            <p v-cloak>
-                @{{ message }}
-            </p>
-            <!-- 要約内容 -->
-            <span v-text="summaryText"></span></br>
-            
-            <!-- 関連ワード3つ表示 -->
-            <input type="text" v-model="word1"></br>
-            <input type="text" v-model="word2"></br>
-            <input type="text" v-model="word3"></br>
 
-            <ul>
-                <li v-for="item in items" v-cloak>
-                    <a v-bind:href="item.url" target="_blank">@{{ item.title }}</a> いいね数: @{{ item.likes_count }}
+        <div id="chat">
+
+            <!-- 入力した単語出力 -->
+            <ul style="list-style: none;">
+                <li v-for="aaa in aaas">
+                    <span>@{{ aaa }}</span>
+                </li>
+            </ul>
+             
+        <span v-text="summaryText"></span></br>
+
+        <!-- QiitaAPI表示 -->
+            <ul style="list-style: none;">
+                <li v-for="item in items">
+                    <a v-bind:href="item.url">@{{ item.title}}</a>
                 </li>
             </ul>
 
+            <input type="text" v-model="word">
+            <button type="button" @click="onClick">要約検索</button></br>
+
+
+            
+
         </div>
-        
         
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.11/lodash.min.js"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
+
     <script>
 
             new Vue({
@@ -36,20 +38,8 @@
                 data: {
                     word: '',
                     summaryText: '',
-                    word1: '',
-                    word2: '',
-                    word3: '',
-                    message: '',
-                    items: null
-                },
-                watch: {
-                    word: function(newWord, oldWord){
-                        this.message = '入力が終わるのを待ってます･･･';
-                        this.debouncedGetAnswer();
-                    }
-                },
-                created: function() {
-                    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000);
+                    items: null,
+                    aaas: []
                 },
                 methods: {
                     onClick: function() { //onclickイベント
@@ -58,41 +48,44 @@
                             'word='+ this.word
                         ];
 
+                        var aaa = this.word;
+                        this.aaas.push(aaa);
+
                         axios.get(url).then((response) => { //ボタン押されたらデータを返す
-
                             this.summaryText = response.data.text;
-                            this.summarys = response.data;
-
-                            this.word1 = response.data.genre;
-                            this.word2 = response.data.genre;
-                            this.word3 = response.data.genre;
-
                         });
-
+                        
+                        var params = { page:1, per_page: 1, query: this.word };
+                        axios.get('https://qiita.com/api/v2/items', {params}).then((response) => {
+                            console.log(response);
+                            this.items = response.data;
+                        });
+ 
                     },
-                    getAnswer: function() {
+                    /*
+                    getAnswer: function(){
                         // キーワードが空の場合はメッセージと検索結果を空にして処理終了
-                            if( this.word === ''){
-                            this.items = null;
-                            this.message = '';
-                            return;
-                            }
-                    
-                            this.message = 'loading...';
-                            var vm = this;
-                            var params = { page:1, per_page: 10, query: this.word };
-                            axios.get('https://qiita.com/api/v2/items', {params})
-                            .then(function(response){
-                                console.log(response);
-                                vm.items = response.data;
-                            })
-                            .catch(function(error){
-                                vm.message = 'Error!' + error;
-                            })
-                            .finally(function(){
-                                vm.message = '';
-                            })
-                    }
+                        if( this.word === ''){
+                        this.items = null;
+                        return;
+                        }
+                
+                        var vm = this;
+                        var params = { page:1, per_page: 3, query: this.word };
+                        axios.get('https://qiita.com/api/v2/items', {params})
+                        .then(function(response){
+                            console.log(response);
+                            vm.items = response.data;
+                        })
+                        .catch(function(error){
+                            console.log('error!') + error;
+                        })
+                        .finally(function(){
+                            console.log('!');
+                        })
+                
+                    },
+                    */
                 },
             });
 
