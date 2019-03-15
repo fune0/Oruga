41968acba913e78e314d41c1067fb837f5eb3e07
田中雅人 <masato.tanaka@di-v.co.jp>
2019-03-15 16:11:48 +0900

    chat構成

--- Oruga/resources/views/summaries/index.blade.php
+++ Oruga/resources/views/summaries/index.blade.php
@@ -19,127 +19,151 @@
 
     <script src="https://cdn.jsdelivr.net/vue/latest/vue.min.js"></script>
     <script src="https://unpkg.com/botui/build/botui.min.js"></script>
+    <script>
+
+    data = @json($lists);
+    console.log(data);
+
+    /* var genreData = data.filter(function(item, index) {
+        if (item.genre == "PHP") return true;
+    });
+    console.log(genreData); */
+
+    /* var wordData = genreData.filter(function(item, index){
+        if (item.word == "Laravel") return true;
+    });
+    console.log(wordData); */
+
+    const botui = new BotUI('bot');
+
+    botui.message.bot({
+      delay: 200,
+      content: 'こんにちは！ Orugaだよ！',
+      loading: true
+    }).then(init);
+
+
+    function init(){
+      botui.message.bot({
+        delay: 800,
+        content: 'ジャンルを選んで',
+        loading: true
+      }).then(() => {
+        return botui.message.bot({
+          delay: 500,
+          loading: true,
+          content: 'わからない単語を入力してね！'
+        }).then(() => {
+          return botui.action.button({
+            delay: 300,
+            action: [{
+              text: 'PHP',
+              value: 'PHP'
+            },{
+              text: 'Ruby',
+              value: 'Ruby'
+            },{
+              text: 'Python',
+              value: 'Python'
+            }]
+          });
+        }).then(res => {
+          genreData = data.filter(function(item, index) { //ジャンルデータに絞り込み
+            if (item.genre == res.text) return true;
+          });
+          console.log(genreData);
+            return botui.message.bot({
+              delay: 400,
+              loading: true,
+              content: 'ジャンルは ' + res.text + 'だね!'
+            });
+        }).then(() => {
+          botui.message.bot({
+            delay: 700,
+            loading: true,
+            content: 'わからない単語を教えて ?'
+          }).then(function () {
+            return botui.action.text({
+              delay: 400,
+              action: {
+                size: 18,
+                icon: 'user-circle-o',
+                sub_type: 'text',
+                placeholder: '単語で入力'
+              }
+            });
+          }).then(res => {
+            name = res.value; // ユーザーの入力値
+            wordData = genreData.filter(function(item, index){
+              // if (item.word == name){
+              //   return true
+              // }else{
+              //   return falseword();
+              // }
+              if(item.word == name) return true;
+            });
+            console.log(wordData);
+
+            if (wordData[0] == null){
+
+              falseword();
+
+            }else{
+
+              botui.message.bot({
+                      delay: 300,
+                      loading: true,
+                      content: wordData[0].text
+              }).then(function() {
+                return botui.message.bot({
+                delay: 300,
+                content: '続けて検索しますか？'
+                })
+              }).then(function() {
+                  //「はい」「いいえ」のボタンを表示
+                  return botui.action.button({
+                      delay: 300,
+                      action: [{
+                        icon: 'circle-thin',
+                        text: 'はい',
+                        value: true
+                      }, {
+                        icon: 'close',
+                        text: 'いいえ',
+                        value: false
+                      }]
+                  });
+              }).then(function(res) {
+              res.value ? init() : end();
+              });
+            }
+          });
+        });
+      });
+    }
+
+
+
+    function falseword() {
+      botui.message.bot({
+        delay: 200,
+        content: 'すいません、要約がありません',
+        loading: true
+      }).then(init);
+    }
+
+
+
+    function end() {
+      botui.message.bot({
+        delay: 200,
+        content: 'ありがとうございました',
+        loading: true
+      });
+    }
+
+
+
+    </script>
   </body>
 </html>
-
-<script>
-
-data = @json($lists);
-console.log(data);
-
-/* var genreData = data.filter(function(item, index) {
-    if (item.genre == "PHP") return true;
-});
-console.log(genreData); */
-
-/* var wordData = genreData.filter(function(item, index){ 
-    if (item.word == "Laravel") return true;
-});
-console.log(wordData); */
-
-const botui = new BotUI('bot');
-
-botui.message.bot({
-  delay: 500,
-  content: 'こんにちは！ Orugaだよ！',
-  loading: true
-}).
-then(() => {
-  return botui.message.bot({
-    delay: 500,
-    loading: true,
-    content: 'ジャンルを選んで、わからない単語を入力してね！' });
-
-}).then(() => {
-  return botui.action.button({
-    delay: 300,
-    action: [
-    {
-      text: 'PHP',
-      value: 'PHP' },
-
-    {
-      text: 'Ruby',
-      value: 'Ruby' },
-
-    {
-      text: 'Python',
-      value: 'Python' }] });
-
-
-
-}).then(res => {
-    genreData = data.filter(function(item, index) { //ジャンルデータに絞り込み
-    if (item.genre == res.text) return true;
-});
-console.log(genreData);
-
-  return botui.message.bot({
-    delay: 400,
-    loading: true,
-    content: 'ジャンルは ' + res.text + 'だね!' });
-
-}).then(() => {
-  botui.message.
-  bot({
-    delay: 700,
-    loading: true,
-    content: 'わからない単語を教えて ?' }).
-
-  then(function () {
-    return botui.action.text({
-      delay: 400,
-      action: {
-        size: 18,
-        icon: 'user-circle-o',
-        sub_type: 'text',
-        placeholder: '? ? ?' } });
-
-
-  }).then(res => {
-    name = res.value; // ユーザーの入力値
-
-    wordData = genreData.filter(function(item, index){ 
-    if (item.word == name) return true;
-});
-    console.log(wordData);
-
-    return botui.message.
-    bot({
-
-            delay: 300,
-            loading: true,
-            content: wordData[0].text
-      });
-
-  }).then(function() {
-    return botui.message.bot({
-      delay: 300,
-      content: '続けて検索しますか？'
-    })
-  }).then(function() {
-      //「はい」「いいえ」のボタンを表示
-      return botui.action.button({
-        delay: 300,
-        action: [{
-          icon: 'circle-thin',
-          text: 'はい',
-          value: true
-        }, {
-          icon: 'close',
-          text: 'いいえ',
-          value: false
-        }]
-      });
-  }).then(function(res) {
-    res.value ? init() : end();
-  });
-
-  function end() {
-    botui.message.bot({
-      content: 'ご利用ありがとうございました！'
-    })
-  }
-
-});
-</script>
